72effad33429a424344412f6c2277b261def217d
angelovercetti99 <angelosobralmarques@gmail.com>
2026-09-24 14:06:17 +0100

    updated

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ketmoss <ketmoss@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/24 13:28:42 by ketmoss           #+#    #+#             */
-/*   Updated: 2026/09/24 14:03:25 by ketmoss          ###   ########.fr       */
+/*   Updated: 2026/09/24 14:05:46 by ketmoss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ void    *ft_memmove(void *dest_str, const void *src_str, size_t n)
     unsigned char *dest;
     unsigned char *src;
 
-    i = n - 1;
+    i = 0;
     dest = (unsigned char*) dest_str;
     src = (unsigned char*) src_str;
     if(dest > src)
     {
+        i = n - 1;
         while (i < n)
             dest[i] = src[i];
                 i--;
